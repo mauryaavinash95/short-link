86fc7dde2325e12cc725094e2ac067f6775f7c8f
Avinash <mauryaavinash95@gmail.com>
2017-08-17 05:11:44 +0530

    Added route handler and Tracker to check auth status

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { Tracker } from 'meteor/tracker';
 // If using react-router >= 4.0.0 use react-router-dom
 // import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -9,6 +10,9 @@ import Login from '../imports/ui/Login';
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
 import NotFound from '../imports/ui/NotFound';
+
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/link'];
 
 const routes = (
   <Router history={browserHistory}>
@@ -18,6 +22,25 @@ const routes = (
     <Route path="*" component={NotFound} />
   </Router>
 );
+
+Tracker.autorun(() => {
+  let isAuthenticated = !!Meteor.userId();
+  const pathname = browserHistory.getCurrentLocation().pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAuthenticatedPage = authenticatedPages.includes(pathname);
+  // console.log("Current user is: ", Meteor.user());
+  // console.log("isAuthenticated: ", isAuthenticated);
+  // console.log("isAuthenticatedPage: ", isAuthenticatedPage);
+  // console.log("isUnauthenticatedPage: ", isUnauthenticatedPage);
+
+  if (isUnauthenticatedPage && isAuthenticated) {
+    browserHistory.push('/link');
+  }
+  else if (isAuthenticated && !isAuthenticated) {
+    browserHistory.push('/');
+  }
+
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Accounts } from 'meteor/accounts-base';
+import { Meteor } from 'meteor/meteor'
+// import { Accounts } from 'meteor/accounts-base';
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -10,8 +11,13 @@ export default class Login extends React.Component {
         }
     }
 
-    onLogin() {
-
+    onLogin(e) {
+        e.preventDefault();
+        let email = this.refs.email.value.trim();
+        let password = this.refs.password.value.trim();
+        Meteor.loginWithPassword({ email }, password, (error) => {
+            console.log('Login callback ', error);
+        })
     }
 
     render() {
@@ -21,7 +27,7 @@ export default class Login extends React.Component {
 
                 {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-                <form onSubmit='onLogin'>
+                <form onSubmit={this.onLogin.bind(this)}>
                     <input type="email" name="email" ref="email" placeholder="Email" />
                     <input type="password" name="password" ref="password" placeholder="Password" />
                     <button>Login</button>
